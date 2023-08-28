6d85ed29bc82c71500776eb71f866522484e8875
marsvillager <1968643693@qq.com>
2023-08-28 10:23:23 +0800

    update

--- auto.py
+++ auto.py
@@ -3,34 +3,42 @@ import re
 import json
 
 from bs4 import BeautifulSoup
+from log import logger
 
 
 if __name__ == '__main__':
-    directory: str = input("Please input the directory of the mindmap: ")
+    directory: str = input("Please input the relative directory(in the ./data directory) of the mindmap: ")
+
     html_title: str = input("Please input the theme of the mindmap: ")
 
     data_file: str = "./data/" + directory + "/" + html_title + ".json"
+    logger.debug("Directory of data file: %s", data_file)
 
-    with open("./模板.html") as f:
-        soup = BeautifulSoup(f, 'html.parser')
+    try:
+        with open("./模板.html") as f:
+            soup = BeautifulSoup(f, 'html.parser')
 
-        # 1. 修改 title 标签
-        title = soup.find("title")
-        title.string = html_title
+            # 1. 修改 title 标签
+            title = soup.find("title")
+            title.string = html_title
 
-        # 2. 定位到 mindmap 的数据区
-        content = soup.find_all("script")[-1]
-        # script_text = content.string
+            # 2. 定位到 mindmap 的数据区
+            content = soup.find_all("script")[-1]
+            # script_text = content.string
 
-        # 3. 以 JSON 格式填写
-        with open(data_file) as json_file:
-            data = json.load(json_file)
+            # 3. 以 JSON 格式填写
+            with open(data_file) as json_file:
+                data = json.load(json_file)
 
-        pattern = r'{\s*"content":\s*"markmap",\s*"depth":\s*\d+,\s*"children":\s*\[\s*\]\s*}'
+            pattern = r'{\s*"content":\s*"markmap",\s*"depth":\s*\d+,\s*"children":\s*\[\s*\]\s*}'
 
-        # 4. 正则表达式匹配 JSON 对象并替换
-        content.string.replace_with(
-            re.sub(pattern, str(data), content.string))
+            # 4. 正则表达式匹配 JSON 对象并替换
+            content.string.replace_with(
+                re.sub(pattern, str(data), content.string))
+
+    except FileNotFoundError:
+        logger.error("Data file not found.")
+        exit()
 
     # 5. 保存
     file_path = "./out/" + directory + "/" + html_title + ".html"
@@ -40,3 +48,5 @@ if __name__ == '__main__':
 
     with open(file_path, 'w') as file:
         file.write(str(soup))
+
+    logger.info("HTML file saved to: %s", file_path)
